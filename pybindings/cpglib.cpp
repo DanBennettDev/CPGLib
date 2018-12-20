cdf9ba40fce8cf0e254c02601e957f0c95fa4cb4
Kappers <thomas.m.kaplan@gmail.com>
2018-12-20 11:23:08 +0000

    pybindings: expand to a functioning test

--- pybindings/cpglib.cpp
+++ pybindings/cpglib.cpp
@@ -1,4 +1,18 @@
 #include "../matsu_signal_calcs.h"
+
+// Wrap the matsu_signal_calcs.h C header in Boost-friendly C++
+struct signal_calcs
+{
+    matsuoka_internals internals;
+
+	static double next_val_rk(double in, double t1, double t2,
+		                     double c1, double c2, double b, double g,
+		                     matsuoka_internals *state)
+    {
+        return  matsuoka_calc_nextVal_RK(in, t1, t2, c1, c2, b, g, state);
+    }
+};
+
 #include <boost/python.hpp>
  
 using namespace boost::python;
@@ -10,6 +24,11 @@ BOOST_PYTHON_MODULE(cpglib)
         .def_readwrite("x2", &matsuoka_internals::x2)
         .def_readwrite("v1", &matsuoka_internals::v1)
         .def_readwrite("v2", &matsuoka_internals::v2)
-        .def_readwrite("out", &matsuoka_internals::out)
+        .def_readwrite("out", &matsuoka_internals::out);
+
+    class_<signal_calcs>("signal_calcs")
+        .def_readwrite("internals", &signal_calcs::internals)
+        .def("next_val_rk", &signal_calcs::next_val_rk)
+        .staticmethod("next_val_rk");
     ;
 }
--- pybindings/test.py
+++ pybindings/test.py
@@ -1,4 +1,16 @@
 import cpglib
+import unittest
 
-mi = cpglib.matsuoka_internals()
-print(dir(mi))
+class MatsuSignalCalcs(unittest.TestCase):
+    def runTest(self):
+        mats_state = cpglib.signal_calcs()
+        next_val = cpglib.signal_calcs.next_val_rk(1, 2, 3, 4, 5, 6, 7, mats_state.internals)
+        assert round(next_val, 2) == -1.62, 'Unexpected RK next step value'
+
+if __name__ == "__main__":
+    # Collect everything in scope which isn't magic, for sanity checking bindings
+    fns = (fn for fn in dir(cpglib) if fn[0:2] != fn[-2:] and fn[-2:] != '__')
+    print('\nImported from cpglib:\n{}'.format('\n'.join(sorted(' - '+f for f in fns))))
+
+    print('\nSanity checking bindings...\n')
+    unittest.main()
